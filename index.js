c74d42236218bd07b6ebeba7c2724eb0fd2a378c
vichearatanak <160566490+vichearatanak@users.noreply.github.com>
2024-02-21 14:01:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -63,9 +63,10 @@ app.delete('/:id', (req, res) => {
     res.send('Item deleted successfully');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+// Export the Express API
 module.exports = app
